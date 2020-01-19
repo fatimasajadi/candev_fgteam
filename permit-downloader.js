40038b4e894c7d96f7db0d6bf1676150e31a4326
fsajadi <s.sfatemeh@gmail.com>
2020-01-19 11:14:48 -0500

    Add result CSV

--- permit-downloader.js
+++ permit-downloader.js
@@ -5,42 +5,41 @@ var fs = require("fs");
 var series = require("async/series");
 
 const baseUrl = "https://apps.cer-rec.gc.ca";
-const dateRegex = /(\d{4}\/\d{2}\/\d{2}, \d+:\d+ [AP]M [A-Z]{3})/m;
 
 scraperjs.StaticScraper.create()
-  .request({
-    uri: `${baseUrl}/REGDOCS/Search/SearchResult?filter=Attr_12629_16&dt=62&sr=1`,
-    secureProtocol: "TLSv1_method",
-    headers: {
-      Cookie: "RDI-NumberOfRecords=500"
-    }
-  })
-  .scrape(function($) {
-    return $("#details-elements > table details summary a")
-      .map(function() {
-        console.log($(this).attr("href"));
-        return baseUrl + $(this).attr("href");
-      })
-      .get();
-  })
-  .then(function(urls) {
-    series(
-      urls.map(url => cb => {
-        const filePath = `${__dirname}/pdfs/${url.match(/(\d+)$/)[1]}.pdf`;
-        let file = fs.createWriteStream(filePath);
+    .request({
+        uri: `${baseUrl}/REGDOCS/Search/SearchResult?filter=Attr_12629_16&dt=62&sr=1`,
+        secureProtocol: "TLSv1_method",
+        headers: {
+            Cookie: "RDI-NumberOfRecords=500"
+        }
+    })
+    .scrape(function($) {
+        return $("#details-elements > table details summary a")
+            .map(function() {
+                console.log($(this).attr("href"));
+                return baseUrl + $(this).attr("href");
+            })
+            .get();
+    })
+    .then(function(urls) {
+        series(
+            urls.map(url => cb => {
+                const filePath = `${__dirname}/pdfs/${url.match(/(\d+)$/)[1]}.pdf`;
+                let file = fs.createWriteStream(filePath);
 
-        request(url, {
-          secureProtocol: "TLSv1_method"
-        })
-          .pipe(file)
-          .on("finish", () => {
-            console.log('DONE: ', filePath);
-            cb();
-          });
-      })
-    );
-  });
+                request(url, {
+                        secureProtocol: "TLSv1_method"
+                    })
+                    .pipe(file)
+                    .on("finish", () => {
+                        console.log('DONE: ', filePath);
+                        cb();
+                    });
+            })
+        );
+    });
 
 String.prototype.clean = function(str) {
-  return this.replace(/\n/g, "").trim();
+    return this.replace(/\n/g, "").trim();
 };
--- permit-parser.js
+++ permit-parser.js
@@ -1,23 +1,19 @@
 const PDFParser = require("pdf2json");
-const fs = require("fs");
 const glob = require("glob-fs")();
-const { PdfReader } = require("pdfreader");
-
-console.log(PdfReader);
 
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 ];
 const monthRegex = `(${months.join("|")})`;
 const dateRegex = `[0-9]+ ${monthRegex} [0-9]{4}`;
@@ -28,44 +24,44 @@ const companyRegex = /^IN THE MATTER OF .+ by (.+) for\s?(.*)\./m;
 
 var files = glob.readdirSync("pdfs/*.pdf", {});
 files.slice(0, 9999999).forEach(file => {
-  const pdfParser = new PDFParser(this, 1);
+    const pdfParser = new PDFParser(this, 1);
 
-  pdfParser.on("pdfParser_dataError", errData =>
-    console.error(errData.parserError)
-  );
-  pdfParser.on("pdfParser_dataReady", pdfData => {
-    const a = pdfParser.getRawTextContent();
-    const normalizedText = a.split(/[\r\n]{2}/).reduce((a, b) => {
-      // console.log("b", b);
-      if (b.trim() === "") {
-        return a + "\n";
-      }
-      return a + (a[a.length - 1] === "\n" ? "" : " ") + b.trim();
-    }, "");
-    const endDate = normalizedText.match(endDateRegex);
-    const startDate = normalizedText.match(startDateRegex);
-    const company = normalizedText.match(companyRegex);
-
-    // console.log(a);
-    if (!startDate || !endDate) {
-    //   console.log("not matched", file);
-      return;
-    }
-    const s = startDate[1];
-    const e = endDate[1];
-    if (new Date(e).getMilliseconds() < new Date(s).getMilliseconds()) {
-      return;
-    }
-    console.log(
-      [
-        file,
-        s,
-        e,
-        `"${company && company[1]}"`,
-        `"${company && company[2]}"`
-      ].join(",")
+    pdfParser.on("pdfParser_dataError", errData =>
+        console.error(errData.parserError)
     );
-  });
+    pdfParser.on("pdfParser_dataReady", pdfData => {
+        const a = pdfParser.getRawTextContent();
+        const normalizedText = a.split(/[\r\n]{2}/).reduce((a, b) => {
+            // console.log("b", b);
+            if (b.trim() === "") {
+                return a + "\n";
+            }
+            return a + (a[a.length - 1] === "\n" ? "" : " ") + b.trim();
+        }, "");
+        const endDate = normalizedText.match(endDateRegex);
+        const startDate = normalizedText.match(startDateRegex);
+        const company = normalizedText.match(companyRegex);
 
-  pdfParser.loadPDF(file);
+        // console.log(a);
+        if (!startDate || !endDate) {
+            //   console.log("not matched", file);
+            return;
+        }
+        const s = startDate[1];
+        const e = endDate[1];
+        if (new Date(e).getMilliseconds() < new Date(s).getMilliseconds()) {
+            return;
+        }
+        console.log(
+            [
+                file,
+                s,
+                e,
+                `"${company && company[1]}"`,
+                `"${company && company[2]}"`
+            ].join(",")
+        );
+    });
+
+    pdfParser.loadPDF(file);
 });
